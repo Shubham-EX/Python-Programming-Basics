982f4ce26c154b53162d209a950278a797a9182c
Shubham Prajapati <114753105+shubham062003@users.noreply.github.com>
2023-12-22 06:56:15 +0530

    Update my_Scikitlearn.py

--- my_Scikitlearn.py
+++ my_Scikitlearn.py
@@ -21,3 +21,42 @@ y_pred model.predict(X_test)
 # Evaluate the model
 
 mse = mean_squared_error(y_test, y_pred) print(f'Mean Squared Error: {mse}')
+
+
+
+
+
+#knn
+
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.metrics import accuracy_score
+
+# Sample data
+
+X = np.random.rand(100, 2)
+
+y = (X[:, 0] + X[:, 1] >1).astype(int)
+
+# Split the data into training and testing sets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+
+test_size=0.2, random_state=42)
+
+# Create a KNN classifier
+
+knn_classifier = KNeighborsClassifier(n_neighbors=3)
+
+# Train the classifier
+
+knn_classifier.fit(X_train, y_train)
+
+# Make predictions
+
+y_pred = knn_classifier.predict(X_test)
+
+# Evaluate the classifier
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
