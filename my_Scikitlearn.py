b12415cd98e75bd2ee46fdf5b791d81ba23be294
Shubham Prajapati <114753105+shubham062003@users.noreply.github.com>
2023-12-22 06:53:57 +0530

    Update my_Scikitlearn.py

--- my_Scikitlearn.py
+++ my_Scikitlearn.py
@@ -6,11 +6,14 @@ Linear Regression from sklearn.model_selection import train_test_split from skle
 
 X = np.random.rand(100, 1) y = 2 * X + 1 + 0.1 * np.random.randn(100, 1)
 
-# Split the data into training and testing sets X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split the data into training and testing sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a linear regression model model = Linear Regression()
+# Create a linear regression model 
+model = Linear Regression()
 
-# Train the model model.fit(X_train, y_train)
+# Train the model 
+model.fit(X_train, y_train)
 
 # Make predictions
 
